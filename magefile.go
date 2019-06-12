96de2ddeac3cb49a34729ef22fb7023f725f13c2
James Benze <benzejaa@gmail.com>
2019-06-12 09:46:36 -0400

    Add build script for static site

--- magefile.go
+++ magefile.go
@@ -5,6 +5,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"os/exec"
 
 	"github.com/magefile/mage/mg"
 	"github.com/magefile/mage/sh"
@@ -31,30 +33,13 @@ func TemplatesBindata() error {
 	})
 }
 
-// A build step that requires additional params, or platform specific steps for example
-/*func Build() error {
-	mg.Deps(InstallDeps)
-	fmt.Println("Building...")
-	cmd := exec.Command("go", "build", "-o", "MyApp", ".")
+func BuildStatic() error {
+	fmt.Println("Building static site")
+	cmd := exec.Command("hugo")
+	cmd.Dir = "static"
+	if mg.Verbose() {
+		cmd.Stdout = os.Stdout
+	}
+	cmd.Stderr = os.Stderr
 	return cmd.Run()
 }
-
-// A custom install step if you need your bin someplace other than go/bin
-func Install() error {
-	mg.Deps(Build)
-	fmt.Println("Installing...")
-	return os.Rename("./MyApp", "/usr/bin/MyApp")
-}
-
-// Manage your deps, or running package managers.
-func InstallDeps() error {
-	fmt.Println("Installing Deps...")
-	cmd := exec.Command("go", "get", "github.com/stretchr/piglatin")
-	return cmd.Run()
-}
-
-// Clean up after yourself
-func Clean() {
-	fmt.Println("Cleaning...")
-	os.RemoveAll("MyApp")
-}*/
