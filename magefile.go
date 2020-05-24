6aa7980b8d4c3b342928a9b7ab01c8d3ca132d5d
James Benze <benzejaa@gmail.com>
2020-05-24 08:08:59 -0400

    Work on static site

--- magefile.go
+++ magefile.go
@@ -24,3 +24,22 @@ func GenerateServerProtoc() error {
 	cmd.Dir = "dynamic"
 	return cmd.Run()
 }
+
+func BuildStatic() error {
+	fmt.Println("Building static site")
+	sitename := "http://localhost:8081"
+	dynamicsite := "https://us-central1-dayton-smackdown-test.cloudfunctions.net"
+	clientId, apiKey := "166144116294-c115t8bqllktva4qp6tvjjeqe7mdggu3.apps.googleusercontent.com", "AIzaSyAJaUR7I6ADbch4OX-WdkjlYsnOrhBx3xU"
+	cmd := exec.Command("hugo")
+	cmd.Env = os.Environ()
+	cmd.Env = append(cmd.Env, "HUGO_BASEURL="+sitename)
+	cmd.Env = append(cmd.Env, "HUGO_DYNAMIC="+dynamicsite)
+	cmd.Env = append(cmd.Env, "HUGO_CLIENT_ID="+clientId)
+	cmd.Env = append(cmd.Env, "HUGO_API_KEY="+apiKey)
+	cmd.Dir = "static"
+	if mg.Verbose() {
+		cmd.Stdout = os.Stdout
+	}
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
