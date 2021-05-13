68907f051d73870554752fdce4fe96c673663ac1
James Benze <benzejaa@gmail.com>
2021-05-13 10:26:21 -0400

    Deploy via CI

--- go.mod
+++ go.mod
@@ -6,15 +6,15 @@ require (
 	github.com/Houndie/toolbox v0.4.0
 	github.com/Microsoft/hcsshim v0.8.16 // indirect
 	github.com/containerd/continuity v0.1.0 // indirect
-	github.com/docker/docker v20.10.6+incompatible
+	github.com/docker/docker v20.10.2+incompatible
 	github.com/golang-migrate/migrate/v4 v4.11.0
 	github.com/golang/protobuf v1.5.0
 	github.com/hashicorp/go-tfe v0.14.0
 	github.com/magefile/mage v1.9.0
 	github.com/moby/buildkit v0.8.2
-	github.com/moby/moby v20.10.6+incompatible
+	github.com/moby/moby v20.10.2+incompatible
 	github.com/moby/sys/mount v0.2.0 // indirect
-	github.com/moby/term v0.0.0-20201216013528-df9cb8a40635 // indirect
+	github.com/moby/term v0.0.0-20201216013528-df9cb8a40635
 	github.com/twitchtv/twirp v5.10.3-0.20200514000016-2bffe5f9b8c3+incompatible
 	go.larrymyers.com/protoc-gen-twirp_typescript v0.0.0-20210107230451-97fd63e543be
 	golang.org/x/sys v0.0.0-20210423185535-09eb48e85fd7 // indirect
---
+++ terraform/netlify.tf
@@ -0,0 +1,68 @@
+variable "github_token" {}
+variable "netlify_token" {}
+
+provider "github" {
+	token = var.github_token
+	owner = "Houndie"
+}
+
+provider "git" {}
+
+provider "netlify" {
+	token = var.netlify_token
+	base_url = "https://api.netlify.com/api/v1/"
+}
+
+data "git_repository" "deploy_tag" {
+	url = "https://github.com/Houndie/dss-registration"
+	tag = var.deploy_version
+}
+
+resource "github_branch" "netlify_deploy" {
+	repository = "dss-registration"
+	branch = "netlify-deploy-${var.workspace}"
+	source_sha = data.git_repository.deploy_tag.commit_sha
+}
+
+resource "netlify_deploy_key" "dayton_swing_smackdown" {}
+
+resource "github_repository_deploy_key" "dayton_swing_smackdown" {
+  title      = "netlify-deploy-${var.workspace}"
+  repository = "dss-registration"
+  key        = netlify_deploy_key.dayton_swing_smackdown.public_key
+  read_only  = false
+}
+
+resource "netlify_site" "dayton_swing_smackdown" {
+	name = "dayton-swing-smackdown-${var.workspace}"
+
+	repo {
+		repo_branch   = github_branch.netlify_deploy.branch
+		command       = <<EOT
+cd static && \
+npm install && \
+GATSBY_BACKEND=https://${heroku_app.dayton_swing_smackdown.name}.herokuapp.com \
+GATSBY_FRONTEND=https://test.daytonswingsmackdown.com \
+GATSBY_CLIENT_ID=${auth0_client.smackdown-website.client_id} \
+GATSBY_AUTH0_DOMAIN=${var.auth0_domain}
+GATSBY_AUTH_AUDIENCE=${auth0_resource_server.smackdown-website.identifier} \
+npx gatsby build
+EOT
+		deploy_key_id = netlify_deploy_key.dayton_swing_smackdown.id
+		dir           = "static/public"
+		provider      = "github"
+		repo_path     = "Houndie/dss-registration"
+	}
+}
+
+resource "github_repository_webhook" "netlify_push" {
+	repository = "dss-registration"
+	events     = ["push", "create"]
+
+	configuration {
+		content_type = "json"
+		url          = "https://api.netlify.com/hooks/github"
+	}
+
+	depends_on = [netlify_site.dayton_swing_smackdown]
+}
