105127baa8bc497c372653eb39ef44e3cdd170fe
James Benze <benzejaa@gmail.com>
2021-08-31 15:39:32 -0400

    Add client side code for vax image upload

---
+++ static/src/components/FormFile.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import {useField, FieldHookConfig} from 'formik'
+import Form from 'react-bootstrap/Form'
+import Col from "react-bootstrap/Col"
+
+type FormFileProps = {
+	label: string
+	as?: 'input' | 'textarea' | 'select'
+	children?: React.ReactNode
+} & FieldHookConfig<any>
+
+export default ({label, ...props}: FormFileProps) => {
+	const [field, meta, helper] = useField(props);
+	return (
+		<Col>
+			<Form.Label htmlFor={props.id || props.name}>{label}</Form.Label>
+			<br />
+			<Form.Control 
+				type="file" 
+				isInvalid={meta.touched && !!meta.error} 
+				name={field.name}
+				as={props.as} 
+				children={props.children} 
+				onChange={(event: React.ChangeEvent<HTMLInputElement>) => { 
+					if (event.currentTarget.files == null || event.currentTarget.files.length == 0) {
+						helper.setValue(undefined)
+						return
+					}
+					helper.setValue(event.currentTarget.files[0])
+				}}/>
+			<Form.Control.Feedback type='invalid' tooltip>{meta.error}</Form.Control.Feedback>
+		</Col>
+	);
+};
