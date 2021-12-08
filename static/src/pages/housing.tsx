329f34c1a5055aff8af6e4ce3bfec8169b5b6691
James Benze <benzejaa@gmail.com>
2021-12-08 15:18:41 -0500

    Add missing pages

---
+++ static/src/pages/housing.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Page from "../components/Page"
+
+export default () => (
+	<Page title="Housing">
+		{() => (
+			<>
+				<p>Dayton Swing Smackdown is pleased to be able to offer registrants a limited amount of housing space with local hosts for no cost.  If local housing is not your style, then we recommend staying at the Hawthorn Suites Hotel, just minutes from downtown Maimisburg, where the Saturday events will take place.</p>
+				<h2>Local Housing</h2>
+				<p>Locals from the Dayton area are offering a limited amount of housing for attendees.  Housing will be provided to individuals in the order in which they registered.  Just indicate you would like housing on your registration form.  If you absolutely can only afford to attend this event if housing is provided, make sure you register as early as possible!</p>
+			</>
+		)}
+	</Page>
+)
