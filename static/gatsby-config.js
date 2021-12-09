43673cc67687aad57e7cfa450081f9f181a33a3a
James Benze <benzejaa@gmail.com>
2021-12-09 13:39:55 -0500

    Add netlify plugin for rendering client paths

--- static/gatsby-config.js
+++ static/gatsby-config.js
@@ -6,5 +6,5 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: ['gatsby-plugin-sass', 'gatsby-plugin-typescript', 'gatsby-plugin-ts-checker'],
+  plugins: ['gatsby-plugin-sass', 'gatsby-plugin-typescript', 'gatsby-plugin-ts-checker', 'gatsby-plugin-netlify'],
 }
